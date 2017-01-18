7832d837d611a89a6507be21fd87e46049307f74
Your Name <name@example.com>
2017-01-18 22:19:36 +0700

    orm symmetric done

--- models/student.js
+++ models/student.js
@@ -1,10 +1,9 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
-    // first_name: DataTypes.STRING,
-    // last_name: DataTypes.STRING,
     bio: DataTypes.TEXT,
-    name:DataTypes.TEXT
+    name:DataTypes.TEXT,
+    address:DataTypes.TEXT
   }, {
     classMethods: {
       associate: function(models) {
