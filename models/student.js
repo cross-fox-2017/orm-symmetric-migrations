14288a780981e36479cdfc211b7ccc3edd5ec7a7
Your Name <name@example.com>
2017-01-18 22:15:22 +0700

    Remove FirstName

--- models/student.js
+++ models/student.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
+    // first_name: DataTypes.STRING,
+    // last_name: DataTypes.STRING,
     bio: DataTypes.TEXT,
     name:DataTypes.TEXT
   }, {
