85cf9cc47382b892f1f6a2f5c1c96caa91a72ee8
Muhammad Iqbal & yoma sofwan <pair+qblol.yomaswn@hacktiv8.com>
2017-01-18 22:08:09 +0700

    Done

--- index.js
+++ index.js
@@ -9,29 +9,31 @@ var db  = require("./models")
 // })
 
 //INSERT 1 VALUE
-db.student.create({
-  first_name:"Eri", last_name:"Irawan", birthdate:"1991-02-11"
-}).then(function(data){
-  console.log("Adding Data", data)
-});
+// db.student.create({
+//   name:"Eri", last_name:"Irawan", birthdate:"1991-02-11"
+// }).then(function(data){
+//   console.log("Adding Data", data)
+// });
 
-//View get fullname
-// db.student.findById(1).then(function(student){
-//   console.log(student.getFullName());
-// });//End get fullname
-//
+//untuk mendapatkan fullname yang nantinya akan dimasukan ke field name
+db.student.findAll().then(function(data){
+  data.forEach(function(data){
+    data.update({name: data.getFullName()})
+  })
+})
+
 // //View getAge
 // db.student.findById(1).then(function(student){
 //   console.log(student.getAge());
 // });//End getAge
 
 //View all data
-db.student.getAllData(function(students){
-  students.forEach(function(student){
-    console.log("ID        : " + student.id);
-    console.log("Firs Name : " + student.first_name);
-    console.log("Last Name : " + student.last_name);
-    console.log("Age       : " + student.getAge());
-    // console.log("Full Name : " + student.getFullName());
-  })
-})
+// db.student.getAllData(function(students){
+//   students.forEach(function(student){
+//     console.log("ID        : " + student.id);
+//     console.log("Firs Name : " + student.first_name);
+//     console.log("Last Name : " + student.last_name);
+//     console.log("Age       : " + student.getAge());
+//     // console.log("Full Name : " + student.getFullName());
+//   })
+// })
--- models/student.js
+++ models/student.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     instanceMethods: {
       getFullName: function() {
-        return (this.first_name + " " + this.last_name)
+        return (this.name + " " + this.last_name)
       },
       getAge: function(){
         let today = new Date();
