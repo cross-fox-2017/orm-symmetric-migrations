cae635a3f77dfbacb86080bae993423d45e8ef5b
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-01-18 20:05:17 +0700

    dgana done all release

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-'use strict'
+"use strict"
 
+const db = require('./models')
+
+db.Student.findAll().then(function(data){
+  data.forEach(function(data){
+    data.update({
+      name: data.getFullName()
+    })
+  })
+})
